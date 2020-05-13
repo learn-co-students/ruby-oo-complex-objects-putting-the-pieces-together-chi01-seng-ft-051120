8ff0568d398ec892a6297e4eddc0f705fc241739
Alex Grimes <alex@alexgrimes.com>
2020-05-13 07:48:44 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,9 +3,15 @@ class Shoe
     attr_reader :brand 
     attr_accessor :color, :size, :material, :condition
 
+    #initialize method
+
     def initialize (brand)
         @brand = brand 
     end 
+
+    # cobble changes condition to new
+    # getting condition undefined :/
+    # self??
 
     def cobble
         puts "Your shoe is as good as new!"
